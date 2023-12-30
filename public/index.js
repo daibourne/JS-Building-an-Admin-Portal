bc608055c47dcee7ab2da0074b9f1ba78a0982cc
daibourne <daijonbourne1005@gmail.com>
2023-12-29 16:19:34 -0800

    Finish Activity

--- public/index.js
+++ public/index.js
@@ -26,3 +26,15 @@ function renderBook(book) {
 }
 
 main()
+
+// let response = await fetch('http://localhost:3001/updateBook',{
+//     method: 'PATCH',
+//     headers: {'Content-Type':'application/json'},
+//     body: JSON.stringify({
+//         'id': 3,
+//         'title': 'Legends of Arathrae',
+//     }),
+// });
+
+// let updatdBook = await response.json();
+// console.log(updatdBook)
